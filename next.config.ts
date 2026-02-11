aeb6de317b8979b999378bbe1ed98ab8b7ab9cca
Emilio Piedra León <espiedra1@utpl.edu.ec>
2026-02-11 16:55:08 -0500

    cambios para migracions a aws

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone', // 👈 AGREGA ESTO: Vital para Docker/VPS
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**.supabase.co', // Permite todas las URLs de Supabase
+        hostname: '**.supabase.co',
       },
     ],
-    // Esto es vital para ahorrar costos en Vercel si usas muchas fotos:
-    // Si quieres que Supabase sirva la foto directa sin que Vercel la toque (más barato):
-    unoptimized: true, 
+    unoptimized: true,
   },
 };
 
